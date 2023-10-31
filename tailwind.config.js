ba8c7696d9dcb83319a35e70908ca77ae5a4dbbb
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-10-31 11:29:57 +0530

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      strongCyan: "hsl(172, 67%, 45%)",
+      veryDarkCyan: "hsl(183, 100%, 15%)",
+      darkGrayishCyan: "hsl(186, 14%, 43%)",
+      grayishCyan: "hsl(184, 14%, 56%)",
+      lightGrayishCyan: "hsl(185, 41%, 84%)",
+      VeryLightGrayishCyan: "hsl(189, 41%, 97%)",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
