eb8e4920ea219375d757a7746c98fef80ba6b1da
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-10-31 12:07:04 +0530

    Create main components scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="rounded-3xl bg-white p-8">
+      <TipForm />
+      <TipDisplay />
+    </div>
+  );
+}
+
+function TipForm() {
+  return <div>INPUT</div>;
+}
+
+function TipDisplay() {
+  return <div>DISPLAY</div>;
 }
