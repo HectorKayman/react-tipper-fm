d7993f81cf644c2fc1165cbdbec3d4f243baa12b
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-11-02 02:58:52 +0530

    Add props to DisplayText and onClick for Reset button

--- src/TipDisplay.jsx
+++ src/TipDisplay.jsx
@@ -1,13 +1,16 @@
 import { DisplayText } from "./DisplayText";
 
-export function TipDisplay() {
+export function TipDisplay({ tipPerPerson, billPerPerson, onReset }) {
   return (
     <section className="flex flex-col gap-10 rounded-2xl bg-veryDarkCyan p-8 mid:gap-40">
       <div className="grid gap-10">
-        <DisplayText>Tip Amount</DisplayText>
-        <DisplayText>Total</DisplayText>
+        <DisplayText amount={tipPerPerson}>Tip Amount</DisplayText>
+        <DisplayText amount={billPerPerson}>Total</DisplayText>
       </div>
-      <button className="mb-2 rounded-md bg-strongCyan py-2 text-2xl text-veryDarkCyan hover:bg-lightGrayishCyan  active:bg-opacity-80">
+      <button
+        className="mb-2 rounded-md bg-strongCyan py-2 text-2xl text-veryDarkCyan hover:bg-lightGrayishCyan  active:bg-opacity-80"
+        onClick={onReset}
+      >
         RESET
       </button>
     </section>
