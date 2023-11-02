19ea34b41e5a26da9392797dcc29df9f4fb1c99b
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-11-03 02:18:19 +0530

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "/react-tipper-fm/",
+  plugins: [react()],
+});
